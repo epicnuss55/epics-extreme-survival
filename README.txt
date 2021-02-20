93f75c07671f921ae3bcce0f01e8dc33a9a300f3
Shayan <epicnuss55@gmail.com>
2021-02-20 19:46:27 +1100

    initial commit

---
+++ README.txt
@@ -0,0 +1,19 @@
+minecraft mod made for 1.16.4 - 35.1.4
+
+THIS MOD IS INCOMPLETE, IF YOU COMPILE EXPECT TO FIND BUGS
+
+//TODO:
+1. start work on the mod
+2. add changelogs to the README.txt
+3. actually complete the mod
+4. add more information in the README.txt
+5. format the README.txt
+6. change the README.txt to a README.md
+//OPTIONAL\\
+7. create a tutorial video on youtube
+
+DD/MM/YY
+//CHANGELOGS
+
+20/02/2021
+initial commit & created mod workspace
---
+++ src/main/java/com/epicnuss55/epicsExtremeSurvival/EpicsExtremeSurvival.java
@@ -0,0 +1,37 @@
+package com.epicnuss55.epicsExtremeSurvival;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("epicsextremesurvival")
+public class EpicsExtremeSurvival {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public EpicsExtremeSurvival() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(FMLServerStartingEvent event) {
+
+    }
+}
